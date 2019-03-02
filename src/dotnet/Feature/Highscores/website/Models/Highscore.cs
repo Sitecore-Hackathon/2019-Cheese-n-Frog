c6e4b15b61236c3768c27756a4e9023fc2f52c64
Gary Wenneker <gary@gary.nu>
2019-03-02 11:46:02 +0100

    GQL working endpoint

---
+++ src/dotnet/Feature/Highscores/website/Models/Highscore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Hackathon.Dinocore.Feature.Highscores.Models
+{
+    public class Highscore
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
---
+++ src/dotnet/Feature/Highscores/website/Providers/DinocoreSchemaProvider.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using GraphQL.Types;
+using Hackathon.Dinocore.Feature.Highscores.Models;
+using Sitecore.Services.GraphQL.Schemas;
+
+namespace Hackathon.Dinocore.Feature.Highscores.Providers
+{
+    public class DinocoreSchemaProvider : SchemaProviderBase
+    {
+        public override IEnumerable<FieldType> CreateRootQueries()
+        {
+            return new FieldType[]
+            {
+                new HighscoreQuery(),
+                new HighscoreMutation()
+            };
+        }
+
+        protected class HighscoreQuery : RootFieldType<HighscoreGraphType, Highscore>
+        {
+            public HighscoreQuery() : base(name: "highscoreQuery", description: "Gets some Highscore data")
+            {
+                var scoreArgument = new QueryArgument(typeof(IntGraphType))
+                {
+                    Name = "score", Description = "The current score"
+                };
+                this.Arguments = new QueryArguments(new [] { scoreArgument });
+            }
+            protected override Highscore Resolve(ResolveFieldContext context)
+            {
+
+                var highscore = new Highscore()
+                {
+                    Created = DateTime.Now,
+                    Name = "GOGOGO",
+                    Score = 87434
+                };
+                return highscore;
+            }
+        }
+
+        protected class HighscoreMutation : RootFieldType<HighscoreGraphType, Highscore>
+        {
+            public HighscoreMutation() : base(name: "highscoreMutation", description: "Sets some Highscore data")
+            {
+                var scoreArgument = new QueryArgument(typeof(IntGraphType))
+                {
+                    Name = "score", Description = "The current score"
+                };
+                this.Arguments = new QueryArguments(new [] { scoreArgument });
+            }
+            protected override Highscore Resolve(ResolveFieldContext context)
+            {
+
+                var highscore = new Highscore()
+                {
+                    Created = DateTime.Now,
+                    Name = "GOGOGO",
+                    Score = 87434
+                };
+                return highscore;
+            }
+        }
+
+        protected class HighscoreGraphType : ObjectGraphType<Highscore>
+        {
+            public HighscoreGraphType()
+            {
+                Name = "Highscore";
+
+                Field<NonNullGraphType<StringGraphType>>("name", resolve: context => context.Source.Name);
+                Field<IntGraphType>("score", resolve: context => context.Source.Score);
+                Field<DateTimeGraphType>("created", resolve: context => context.Source.Created);
+            }
+        }
+    }
+}
